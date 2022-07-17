90544a769d81419ab3688da7d553148ae2ca4804
indiedanish <danishbuttdb@hotmail.com>
2022-07-17 18:48:00 +0500

    Completed 2

--- node.js
+++ node.js
@@ -6,7 +6,19 @@ const express = require("express");
 var indexRouter = require('./routes/index');
 var cors = require("cors"); //use this
 var app = express();
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(cors()); //and this
